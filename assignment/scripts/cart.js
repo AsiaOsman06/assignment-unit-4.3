cb7d0d19e3d7097cf9a3387d34337b0747dec44a
Asia Osman <asiaosman06@gmail.com>
2024-03-17 17:45:10 -0500

    functions

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,17 +2,53 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
+
+ 
 
 
 
+//let items = []; // Define an empty array to store items
+
+// Function to add an item to the array
+function addItem(item) {
+    basket.push(item); // Push the item into the array
+    return true;
+}
+
+// Example usage:
+addItem("apple");
+addItem("banana");
+addItem("orange");
+
+console.log(`${basket}`); // Output: ["apple", "banana", "orange"]
+
+function listItems(){
+    for(i = 0; i< basket.length; i++){
+        console.log(basket[i]);
+    }
+}
+console.log(listItems());
 
 
+function empty(){
+    while(basket.length>0){
+    basket.pop();
+    }
+
+}
+console.log(empty());
+console.log(`${basket}`);
+// Stretch Goals
+const maxItems = 5;
 
 
-
-
-
-// DO NOT MODIFY
+function isFull(){
+        return false;
+    } 
+    
+// DO NOT 
+//MODIFY
 // Used for automated testing
 try {
     module.exports = {
